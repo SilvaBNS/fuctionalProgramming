a9e013b74e88831c42e1bddcac14afdaaac8c6ff
SilvaBNS <136633217+SilvaBNS@users.noreply.github.com>
2023-07-03 23:06:37 +0530

    Update Q3.scala

--- Q3.scala
+++ Q3.scala
@@ -5,17 +5,27 @@ object Q3 {
     val result1=toUpper(input1)
     println(result1)
     println("")
+    
     print("Enter the input string2 : ")
     val input2 = scala.io.StdIn.readLine()
-    val result2 = toLower(input2)
+    val result2 = formatNames (input2, str => str.head + (str.charAt(1).toUpper + str.drop(2)))
     println(result2)
     println("")
+    
+    print("Enter the input string3 : ")
     val input3 = scala.io.StdIn.readLine()
     val result3 = toLower(input3)
     println(result3)
     println("")
-
+    
+    print("Enter the input string4 : ")
+    val input4 = scala.io.StdIn.readLine()
+    val result4= formatNames (input4, str => str.init + str.last.toUpper)
+    println(result4)
+    println("")
   }
+
+  
   def toUpper(str : String) : String={
     val upper=str.toUpperCase()
     return upper
@@ -26,14 +36,9 @@ object Q3 {
     return lower
   }
 
-  /*def formatNames(name: String)(formatFunc: String => String): String = {
+
+  def formatNames(name: String,formatFunc: String => String): String = {
     formatFunc(name)
   }
-
-  def formatFunc(name : String) : String ={
-    val editedName = name.capitalize
-
-  }*/
-
 
 }
